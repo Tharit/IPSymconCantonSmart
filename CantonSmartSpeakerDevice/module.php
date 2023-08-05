fd49d5ed525acadefd2060defd8c107548c0151c
Martin Kleinhans <kleinhans.martin@bcg.com>
2023-08-05 22:33:55 +0200

    first commit

---
+++ CantonSmartSpeakerDevice/module.php
@@ -0,0 +1,224 @@
+<?php
+
+require_once(__DIR__ . '/../libs/ModuleUtilities.php');
+
+class CantonSmartSpeakerDevice extends IPSModule
+{
+    use ModuleUtilities;
+    
+    public function Create()
+    {
+        //Never delete this line!
+        parent::Create();
+
+        $this->RequireParent('{3CFF0FD9-E306-41DB-9B5A-9D06D38576C3}'); // IO Client Socket
+
+        // variables
+        $this->RegisterVariableBoolean("Connected", "Connected");
+        $this->RegisterVariableString("Source", "Source");
+        $this->RegisterVariableString("Application", "Application");
+        $this->RegisterVariableString("State", "State");
+        $this->RegisterVariableString("Artist", "Artist");
+        $this->RegisterVariableString("Album", "Album");
+        $this->RegisterVariableString("Title", "Title");
+        $this->RegisterVariableInteger("Position", "Position");
+        $this->RegisterVariableInteger("Duration", "Duration");
+        $this->RegisterVariableString("Cover", "Cover");
+        $this->RegisterVariableInteger("Volume", "Volume", "~Intensity.100");
+        $this->EnableAction("Volume");
+        $this->RegisterVariableString("Input", "Input");
+        $this->RegisterVariableBoolean("PowerState", "PowerState");
+
+        // messages
+        $this->RegisterMessage(0, IPS_KERNELSTARTED);
+        $this->RegisterMessage($this->InstanceID, FM_CONNECT);
+        $this->RegisterMessage($this->InstanceID, FM_DISCONNECT);
+
+        // clear state on startup
+        $this->ResetState();
+
+        // if this is not the initial creation there might already be a parent
+        if($this->UpdateConnection() && $this->HasActiveParent()) {
+            $this->SendDebug('Module Create', 'Already connected', 0);
+        }
+    }
+
+    /**
+     * Configuration changes
+     */
+    public function ApplyChanges()
+    {
+        $parentID = $this->GetConnectionID();
+
+        parent::ApplyChanges();
+
+        if (!IPS_GetProperty($parentID, 'Open')) {
+            IPS_SetProperty($parentID, 'Open', true);
+            @IPS_ApplyChanges($parentID);
+        }
+    }
+
+    public function MessageSink($TimeStamp, $SenderID, $Message, $Data)
+    {
+        switch ($Message) {
+            case IPS_KERNELSTARTED:
+            case FM_CONNECT:
+                $this->SendDebug('STARTED / CONNECT', 'resetting connection', 0);
+                // if new parent and it is already active: connect immediately
+                if($this->UpdateConnection() && $this->HasActiveParent()) {
+                    $this->ApplyChanges();
+                }
+                break;
+            case FM_DISCONNECT:
+                $this->ResetState();
+                $this->UpdateConnection();
+                break;
+            case IM_CHANGESTATUS:
+                // reset state
+                $this->ResetState();
+
+                $this->SendDebug('CHANGESTATUS', json_encode($Data), 0);
+
+                // if parent became active: connect
+                if ($Data[0] === IS_ACTIVE) {
+                    $this->SetValue("Connected", true);
+                    $this->Connect();
+                } else {
+                    $this->SetValue("Connected", false);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public function Connect() {
+        // request input
+        // request volume
+        // request power state
+        $data = $this->MakePacket(0x03, 0x02) . $this->MakePacket(0x0c, 0x02) . $this->MakePacket(0x06, 0x02);
+        $this->SendDebug('Sending Data', bin2hex($data), 0);
+        CSCK_SendText($this->GetConnectionID(), $data);
+    }
+
+    private function MakePacket($property, $type, $value = "") {
+        if($value) {
+            $length = pack('n', strlen($value));
+        } else {
+            $value = '';
+            $length = '';
+        }
+        $property = chr($property);
+        $type = chr($type);
+        return "\xff\xaa\x00$property$type$length$value";
+    }
+
+    public function ReceiveData($data) {
+        // unpack & decode data
+        $data = json_decode($data);
+        $data = utf8_decode($data->Buffer);
+
+        $data = $this->MUGetBuffer('Data') . $data;
+
+        if(strlen($data) >= 7) {
+            // ff   aa   00   03   01   00   03
+            $len = unpack('n', $data, 5);
+            $property = unpack('n', $data, 2);
+            $type = $data[4];
+            if(strlen($data) >= 7 + $len) {
+                $this->SendDebug('Processing Packet', bin2hex(substr($data, 0, 7 + $len), 0));
+
+                // power
+                if($property == 0x06) {
+                    $this->SetValue('PowerState', ord($data[7]));
+                // input
+                } else if($property == 0x03) {
+                    $value = ord($data[7]);
+                    // ff   aa   00   03   01   00   03   01   10   01 => ATV
+                    // ff   aa   00   03   01   00   03   02   04   01 => SAT
+                    // ff   aa   00   03   01   00   03   03   0e   01 => PS
+                    // ff   aa   00   03   01   00   03   06   02   01 => TV
+                    // ff   aa   00   03   01   00   03   07   06   01 => CD
+                    // ff   aa   00   03   01   00   03   0b   06   01 => DVD
+                    // ff   aa   00   03   01   00   03   0f   12   01 => AUX
+                    // ff   aa   00   03   01   00   03   17   13   01 => NET
+                    // ff   aa   00   03   01   00   03   15   14   01 => BT
+                    switch($value) {
+                        case 0x01: $value = 'ATV'; break;
+                        case 0x02: $value = 'SAT'; break;
+                        case 0x03: $value = 'PS'; break;
+                        case 0x06: $value = 'TV'; break;
+                        case 0x07: $value = 'CD'; break;
+                        case 0x0b: $value = 'DVD'; break;
+                        case 0x0f: $value = 'AUX'; break;
+                        default: 
+                        case 0x17: $value = 'NET'; break;
+                        case 0x15: $value = 'BT'; break;
+                    }
+                    $this->SetValue('Input', $value);
+                // volume
+                } else if($property = 0x0c) {
+                    if($len == 2) {
+                        $this->SetValue('Volume', ord($data[7]));
+                    }
+                }
+                $data = substr($data, 7 + $len);
+            }
+        }
+
+        $this->MUSetBuffer('Data', $data);
+    }
+
+    public function RequestAction($ident, $value)
+    {
+        if($ident === 'Volume') {
+            // @TODO
+        }
+
+        $this->SendDebug('Action', $ident, 0);
+    }
+
+    //------------------------------------------------------------------------------------
+    // external methods
+    //------------------------------------------------------------------------------------
+    public function SetVolume(int $volume) {
+        // @TODO
+    }
+
+    public function Play() {
+        // @TODO
+    }
+
+    public function Pause() {
+        // @TODO
+    }
+
+    public function Stop() {
+        // @TODO
+    }
+
+    public function Next() {
+        // @TODO
+    }
+
+    public function Prev() {
+        // @TODO
+    }
+
+    //------------------------------------------------------------------------------------
+    // module internals
+    //------------------------------------------------------------------------------------
+    private function ResetState() {
+        $this->SetValue("Artist", '-');
+        $this->SetValue("Album", '-');
+        $this->SetValue("Title", '-');
+        $this->SetValue("Cover", "");
+        $this->SetValue("Source", "-");
+        $this->SetValue("Application", "-");
+        $this->SetValue("Duration", 0);
+        $this->SetValue('Source', '-');
+        $this->SetValue('State', 'stop');
+        $this->SetValue('Input', 'NET');
+        $this->SetValue('PowerState', false);
+    }
+}
---
+++ test.php
@@ -0,0 +1,302 @@
+<?php
+
+function MakePacket($property, $type, $value = "") {
+    if($value) {
+        $length = pack('n', strlen($value));
+    } else {
+        $value = '';
+        $length = '';
+    }
+    $property = chr($property);
+    $type = chr($type);
+    return "\xff\xaa\x00$property$type$length$value";
+}
+
+var_dump(bin2hex(MakePacket(0x0c, 0x01, "\x23")));
+return;
+
+$sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+socket_connect($sock, '10.0.125.96', 7777);
+
+$buffer = "\x00\x00\x02\x03\x00\x00\x00\x00\x0d\x0010.0.89.246";
+
+socket_send($sock, $buffer, 23,0);
+$bytes = socket_recv($sock, $buffer, 1024, 0);
+//$buffer2 = "\x00\x00\x02\x28\x00\x00\x0\x00\x00\x00";
+//$buffer2 = "\x00\x00\x02\x33\x00\x00\x00\x00\x01\x002";
+$buffer2 = "\x00\x00\x02\x28\x00\x00\x00\x00\x04\x00STOP";
+//$buffer2 = "\x00\x00\x02\x33\x00\x00\x00\x00\x01\x00\x33";
+            socket_send($sock, $buffer2, 14,0);
+
+while(true) {
+    $bytes = socket_recv($sock, $buffer, 1024, 0);
+    $remaining = $bytes;
+    
+    $type = ord($buffer[2]);
+        $cmd = unpack('n', $buffer, 3)[1];
+        $len = unpack('n', $buffer, 8)[1];
+        
+        echo "Action: " . $type ." | CMD: " . $cmd . "\n";
+        
+        // refresh playback state
+        if($type == 2 && $cmd == 40) {
+            $buffer2 = "\x00\x00\x01\x28\x00\x00\x00\x00\x00\x00";
+            socket_send($sock, $buffer2, 10,0);
+        }
+    
+
+    var_dump(bin2hex($buffer));
+}
+
+
+/*
+
+InputSource:
+0 => NET
+2 => BT
+NONE => all others
+
+*/
+
+/*
+
+// receive playback update
+// playing
+00000200330087bd000130
+// paused
+000002003300a7ff000132
+// nothing
+000002003300979c000131
+
+// receive position update
+000002003100d6260006313538303832
+
+
+// mute (from server)
+000002003f00f0ac00044d555445
+
+// control stream
+
+00 00 02 28 00 00 00 00 04 00 S T O P
+00 00 02 28 00 00 00 00 04 00 P R E V
+00 00 02 28 00 00 00 00 04 00 N E X T
+00 00 02 28 00 00 00 00 05 00 P A U S E
+00 00 02 28 00 00 00 00 06 00 R E S U M E
+
+
+// set streaming volume
+
+$sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+socket_connect($sock, '192.168.8.234', 7777);
+
+$buffer = "\x00\x00\x02\x03\x00\x00\x00\x00\x0d\x00192.168.8.182";
+
+socket_send($sock, $buffer, 23,0);
+
+socket_recv($sock, $buffer, 1024, 0);
+echo(bin2hex($buffer));
+echo "\n\n";
+
+
+$buffer = "\x00\x00\x02\x40\x00\x00\x00\x00\x02\x00\x21\x34"; // volume in percentage of 70
+socket_send($sock, $buffer, 12,0);
+socket_close($sock);
+return;
+*/
+
+$sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+socket_connect($sock, '10.0.125.96', 50006);
+
+// --   --   --   --   --   len  len  val
+// ff   aa   00   0c   01   00   01   vv
+
+// client: set volume
+// only works on none streaming input channels.. 
+// otherwise have to use port 7777 interface
+// ff   aa   00   0c   01   00   01   31
+
+// client: request volume
+// ff   aa   00   0c   02   00   00
+
+// server: update volume
+//                                    vol  
+// ff   aa   00   0c   01   00   02   2b   32
+// ff   aa   00   0c   01   00   02   1a   32
+
+// client: request power state
+// ff   aa   00   06   02   00   00
+
+// client: update power state
+//                                    pwr
+// ff   aa   00   06   01   00   01   00
+
+
+// client: request input
+// ff   aa   00   03   02   00   00
+
+// client: set input
+
+
+// client: request mute
+// ff   aa   00   09   02   00   00
+
+// client: set mute
+//                                   mute
+// ff   aa   00   09   01   00   01  00
+
+// server: update mute
+//                                   mute
+// ff   aa   00   09   01   00   01  00
+
+// server: updateinput
+// last byte: play mode
+// ff   aa   00   03   01   00   03   01   10   01 => ATV
+// ff   aa   00   03   01   00   03   02   04   01 => SAT
+// ff   aa   00   03   01   00   03   03   0e   01 => PS
+// ff   aa   00   03   01   00   03   06   02   01 => TV
+// ff   aa   00   03   01   00   03   07   06   01 => TV
+// ff   aa   00   03   01   00   03   0b   06   01 => DVD
+// ff   aa   00   03   01   00   03   0f   12   01 => AUX
+// ff   aa   00   03   01   00   03   17   13   01 => NET
+// ff   aa   00   03   01   00   03   15   14   01 => BT
+
+// client: request ???
+// ff   aa   00   0c   04   00   00
+
+
+//$buffer = "\xff\xaa\x00\x06\x02\x00\x00";//\xff\xaa\x00\x0c\x02\x00\x00\xff\xaa\x00\x03\x02\x00\x00";
+//$buffer = "\xff\xaa\x00\x03\x01\x00\x03\x02\x04\x01";//\xff\xaa\x00\x0c\x02\x00\x00\xff\xaa\x00\x03\x02\x00\x00";
+//$buffer = "\xff\xaa\x00\x03\x02\x00\x00";//\xff\xaa\x00\x0c\x02\x00\x00\xff\xaa\x00\x03\x02\x00\x00";
+
+//$buffer = "\xff\xaa\x00\x0c\x02\x00\x00\xff\xaa\x00\x04\x02\x00\x00\xff\xaa\x00\x06\x02\x00\x00";
+//$buffer = "\xff\xaa\x00\x0c\x01\x00\x01\x16";
+
+//socket_send($sock, $buffer, 10, 0);
+
+while(true) {
+    $bytes = socket_recv($sock, $buffer, 1024, 0);
+    $remaining = $bytes;
+    
+    var_dump(bin2hex($buffer));
+}
+
+$sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+socket_connect($sock, '192.168.8.234', 7777);
+
+
+// register with ip
+
+$buffer = "\x00\x00\x02\x03\x00\x00\x00\x00\x0d\x00192.168.8.182";
+
+echo(bin2hex($buffer));
+echo "\n\n";
+
+socket_send($sock, $buffer, 23,0);
+
+socket_recv($sock, $buffer, 1024, 0);
+echo(bin2hex($buffer));
+echo "\n\n";
+
+// request model
+
+$buffer = "\x00\x00\x02\xd0\x00\x00\x00\x00\x0a\x00READ_Model";
+echo(bin2hex($buffer));
+echo "\n\n";
+socket_send($sock, $buffer, 20,0);
+
+socket_recv($sock, $buffer, 1024, 0);
+echo(bin2hex($buffer));
+echo $buffer;
+
+
+echo "\n----\n";
+// request volume 40
+// request device status 70
+// request play window status 2d
+// request MRA info?? 46
+// request ?? 97
+// request ?? 2a
+$buffer = "\x00\x00\x01\x40\x00\x00\x00\x00\x00\x00";
+$buffer .= "\x00\x00\x01\x97\x00\x00\x00\x00\x00\x00";
+$buffer .= "\x00\x00\x01\x2a\x00\x00\x00\x00\x00\x00";
+echo(bin2hex($buffer));
+echo "\n\n";
+socket_send($sock, $buffer, 30,0);
+
+/*socket_recv($sock, $buffer, 1024, 0);
+echo(bin2hex($buffer));
+echo $buffer;
+echo "\n\n";
+
+
+// request ?? 97
+$buffer = "\x00\x00\x01\x97\x00\x00\x00\x00\x00\x00";
+echo(bin2hex($buffer));
+echo "\n\n";
+socket_send($sock, $buffer, 10,0);
+
+socket_recv($sock, $buffer, 1024, 0);
+echo(bin2hex($buffer));
+echo $buffer;
+
+echo "\n\n";
+
+// request ?? 2a
+$buffer = "\x00\x00\x01\x2a\x00\x00\x00\x00\x00\x00";
+echo(bin2hex($buffer));
+echo "\n\n";
+socket_send($sock, $buffer, 10,0);
+
+socket_recv($sock, $buffer, 1024, 0);
+echo(bin2hex($buffer));
+echo $buffer;
+
+echo "\n\n";
+
+
+*/
+$buffer = "\x00\x00\x02\x29\x00\x00\x00\x00\x0a\x00GETUI:PLAY";
+echo(bin2hex($buffer));
+echo "\n\n";
+socket_send($sock, $buffer, 20,0);
+
+
+while(true) {
+    $bytes = socket_recv($sock, $buffer, 1024, 0);
+    $remaining = $bytes;
+    
+    do {
+        $type = ord($buffer[2]);
+        $cmd = unpack('n', $buffer, 3)[1];
+        $len = unpack('n', $buffer, 8)[1];
+        
+        echo "Action: " . $type ." | CMD: " . $cmd . "\n";
+        
+        // refresh device status when notified
+        if($type == 2 && $cmd == 112) {
+            $buffer2 = "\x00\x00\x01\x70\x00\x00\x00\x00\x00\x00";
+            socket_send($sock, $buffer2, 10,0);
+        // received new device status info
+        } else if($type == 1 && $cmd == 112) {
+            if($len) {
+                echo 'Device Status: ' . substr($buffer, 10, $len) . '\n';
+            }
+        } else if($type == 1 && $cmd == 64) {
+            if($len) {
+                echo 'Volume: ' . substr($buffer, 10, $len) . '\n';
+            }
+        } else if($type == 1 && $cmd == 42) {
+            if($len) {
+                echo 'test: ' . substr($buffer, 10, $len) . '\n';
+            }
+        } 
+        
+        $remaining -= 10 + $len;
+        $buffer = substr($buffer, 10 + $len, $remaining);
+    } while($remaining > 0);
+
+    //echo(bin2hex($buffer));
+
+    
+    echo "\n\n";
+};
