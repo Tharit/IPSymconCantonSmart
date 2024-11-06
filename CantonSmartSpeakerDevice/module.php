db6ec8b919587af919e7b47bc93683edca2fffe9
Martin Kleinhans <kleinhans.martin@bcg.com>
2024-11-06 16:45:12 +0100

    fixed coverart

--- CantonSmartSpeakerDevice/module.php
+++ CantonSmartSpeakerDevice/module.php
@@ -580,10 +580,10 @@ class CantonSmartSpeakerDevice extends IPSModule
             }
 
             while(strlen($buffer) >= 7) {
-                $len = unpack('n', $data, 5)[1];
-                $property = unpack('n', $data, 2)[1];
-                $type = ord($data[4]);
-                if(strlen($data) >= 7 + $len) {
+                $len = unpack('n', $buffer, 5)[1];
+                $property = unpack('n', $buffer, 2)[1];
+                $type = ord($buffer[4]);
+                if(strlen($buffer) >= 7 + $len) {
                     if($property == 0x03) {
                         $input = ord($buffer[7]);
                     }
